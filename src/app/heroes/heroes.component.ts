c560fbafaf70453cb9296ac056133e716b3b2107
cleitonjesus <cleiton2003@gmail.com>
2018-07-08 15:40:08 -0300

    Finalizado rota

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,8 +11,6 @@ export class HeroesComponent implements OnInit {
   
   heroes: Hero[];
 
-  selectedHero: Hero;
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {    
@@ -23,9 +21,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
 }
